b1cc4f722e0a68f4b2688744aa48cf6c64bce8bb
ChrisAnd0 <122125567+ChrisAnd0@users.noreply.github.com>
2023-01-20 12:59:38 -0500

    Add files via upload
    updated files to match new server paths

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -1,4 +1,4 @@
 <footer>
 	<p>&copy;Chris Omahen, 2017<br />
-	<a href="http://kelvin.ist.rit.edu/~cjo3760/final/about.php">About</a></p>
+	<a href="https://chrisomahen.com/rit-interests//about.php">About</a></p>
 </footer>
--- assets/inc/header.php
+++ assets/inc/header.php
@@ -6,34 +6,34 @@
 <!-- Main navigation-->
 <nav>
 	<div class="navButton">
-		<p><a href="http://kelvin.ist.rit.edu/~cjo3760/final/index.php">Home</a></p>
+		<p><a href="https://chrisomahen.com/rit-interests//index.php">Home</a></p>
 	</div>
 	
 	<div class="navButton" onmouseout="hideMenu(this)" onmouseover="displayMenu(this)">
-		<p><a href="http://kelvin.ist.rit.edu/~cjo3760/final/on/index.php">On-Campus</a></p>
+		<p><a href="https://chrisomahen.com/rit-interests//on/index.php">On-Campus</a></p>
 		
 		<ul class="navMenu">
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/on/dtunnel.php">Dorm Tunnels</a></li>
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/on/atunnel.php">Academic Tunnels</a></li>
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/on/outdoor.php">Getting Around Outdoors</a></li>
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/on/study.php">Study Areas</a></li>
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/on/bus.php">Taking the Bus</a></li>
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/on/drive.php">Driving and Parking</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//on/dtunnel.php">Dorm Tunnels</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//on/atunnel.php">Academic Tunnels</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//on/outdoor.php">Getting Around Outdoors</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//on/study.php">Study Areas</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//on/bus.php">Taking the Bus</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//on/drive.php">Driving and Parking</a></li>
 		</ul>
 	</div>
 	
 	<div class="navButton" onmouseout="hideMenu(this)" onmouseover="displayMenu(this)">
-		<p><a href="http://kelvin.ist.rit.edu/~cjo3760/final/off/index.php">Off-Campus</a></p>
+		<p><a href="https://chrisomahen.com/rit-interests//off/index.php">Off-Campus</a></p>
 		
 		<ul class="navMenu">
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/off/parkpoint.php">Park Point</a></li>
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/off/jefferson.php">Jefferson Road</a></li>
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/off/marketplace.php">Marketplace Mall</a></li>
-			<li><a href="http://kelvin.ist.rit.edu/~cjo3760/final/off/north.php">Heading North</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//off/parkpoint.php">Park Point</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//off/jefferson.php">Jefferson Road</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//off/marketplace.php">Marketplace Mall</a></li>
+			<li><a href="https://chrisomahen.com/rit-interests//off/north.php">Heading North</a></li>
 		</ul>
 	</div>
 	
 	<div class="navButton">
-		<p><a href="http://kelvin.ist.rit.edu/~cjo3760/final/comments.php">Comments</a></p>
+		<p><a href="https://chrisomahen.com/rit-interests//comments.php">Comments</a></p>
 	</div>
 </nav>
